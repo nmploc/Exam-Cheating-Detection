6a00a513c27bc69f891e424f4da194663ae65146
T123SQ <92706910+T123SQ@users.noreply.github.com>
2023-07-01 10:57:36 +0700

    add pose estimation pre-trained model to test_cam

--- test_cam.py
+++ test_cam.py
@@ -2,13 +2,17 @@ import cv2
 from ultralytics import YOLO 
 
 model = YOLO('yolov8m.pt')
+model2 = YOLO('yolov8m-pose.pt')
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read() 
     result = model(frame, save = False)
-    print(result)
+    result2 = model2(frame, save = False)
+    print(result[0])
+    print(result2[0])
     frame = result[0].plot()
+    frame = result2[0].plot()
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
         break
