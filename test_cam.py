0a3ffdf2aa85f98937b058a1e1c0927207bd7fde
T123SQ <92706910+T123SQ@users.noreply.github.com>
2023-07-05 10:09:02 +0700

    nothing here

--- test_cam.py
+++ test_cam.py
@@ -1,15 +1,17 @@
 import cv2 
 from ultralytics import YOLO 
+import time 
 
 #model = YOLO('yolov8m.pt')
-model2 = YOLO('yolov8m-pose.pt')
- 
-model2.to('cuda')
+model2 = YOLO('yolov8n-pose.pt')
 cap = cv2.VideoCapture(0)
+
+prev_frame_time = 0
+new_frame_time = 0
 
 while True:
     ret, frame = cap.read() 
-    fps =  int(cap.get(cv2.CAP_PROP_FPS))
+    
     #bounding_boxes = model(frame)
     #print(bounding_boxes)
     #result = model(frame, save = False)
@@ -18,6 +20,10 @@ while True:
     print(result2[0])
     #frame = result[0].plot()
     frame = result2[0].plot()
+    new_frame_time = time.time()
+    fps = 1/(new_frame_time-prev_frame_time)
+    prev_frame_time = new_frame_time
+    fps = int(fps)
     cv2.putText(frame, str(fps), (50,50), cv2.FONT_HERSHEY_COMPLEX , 1,  (0,255,0), 2)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
