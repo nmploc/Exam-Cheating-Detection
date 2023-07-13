ac9fef4fe832278aea0e7a9bd1972483e606418e
T123SQ <92706910+T123SQ@users.noreply.github.com>
2023-07-13 10:31:16 +0700

    change getBB from single to multi person

--- getBB.py
+++ getBB.py
@@ -1,18 +1,18 @@
-from ultralytics import YOLO
 import cv2
-import time
+from ultralytics import YOLO 
+import torch
 
-def get_bounding_box(frame):
-    model = YOLO('yolov8m.pt')
-    # Capture a frame
+if torch.cuda.is_available():
+    device = 0
+else:
+    device = 'cpu'
+def get_bounding_boxes(frame):
     model = YOLO('yolov8n.pt')
-
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = model.predict(img, device = 0)
-    x1 = 0 
-    y1 = 0 
-    x2 = 0 
-    y2 = 0 
+    results = model.predict(img, device = device)
+
+    bounding_boxes = []  # Danh sách để lưu trữ tọa độ của bounding boxes
+
     # Find the bounding box coordinates
     for r in results:
         boxes = r.boxes
@@ -20,27 +20,12 @@ def get_bounding_box(frame):
             c = box.cls
             if model.names[int(c)] == 'person':
                 b = box.xyxy[0]  # Get the bounding box coordinates
-                x1 = int(b[0]) #x1
-                y1 = int(b[1]) #y1
-                x2 = int(b[2]) #x2
-                y2 = int(b[3]) #y2
+                x1= int(b[0])
+                y1 = int(b[1])
+                x2 = int(b[2])
+                y2 = int(b[3])
+                bounding_box = (x1, y1, x2, y2)
+                bounding_boxes.append(bounding_box)
 
-    # Return the bounding box coordinates
-    return x1, y1, x2, y2
-
-if __name__ == "__main__":
-    cap = cv2.VideoCapture(0)
-    while True:
-        ret, frame = cap.read()
-        x1, y1, x2, y2 = get_bounding_box(frame)
-        print(str(x1) + " " + str(y1) + " " + str(x2) +" "+ str(y2))
-        roi = frame[y1:y2, x1:x2]
-        cv2.imshow("Roi",roi)
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,0), 2)
-        cv2.imshow("Frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        
-    # Release video capture and destroy windows
-    cap.release()
-    cv2.destroyAllWindows()
+    # Return the list of bounding box coordinates
+    return bounding_boxes
